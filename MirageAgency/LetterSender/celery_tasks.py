0456308ef64a0d104e68979b66a871224aaf263d
Alexandr <sasha.v720@gmail.com>
2023-11-20 13:58:37 +0200

    test version for deploy

--- MirageAgency/LetterSender/celery_tasks.py
+++ MirageAgency/LetterSender/celery_tasks.py
@@ -1,16 +1,6 @@
-import re
-import bs4
-import time
-
-from celery import shared_task
-from fake_useragent import UserAgent
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from MirageAgency.celery import app
 
 
-@shared_task
-def get_online_id(username, password):
-    all_id = [username, password]
-    return all_id
+@app.task
+def get_online_id(a, b):
+    return a+b

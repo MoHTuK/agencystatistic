3ff58d46fbb5f8f087ece8c4d8c0f5be0178917c
Alexandr <sasha.v720@gmail.com>
2024-02-04 15:13:40 +0200

    Only statiсstic 15.04(04.02.24)

--- MirageAgency/MirageAgency/settings.py
+++ MirageAgency/MirageAgency/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Statistics.apps.StatisticsConfig',
-    'LetterSender.apps.LettersenderConfig'
 ]
 
 MIDDLEWARE = [

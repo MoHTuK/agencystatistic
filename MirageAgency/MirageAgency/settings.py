e15bf8f119f40da5e94bd7b7cccc2b99d3502d0d
Alexandr <sasha.v720@gmail.com>
2024-02-04 16:18:10 +0200

    prod up

--- MirageAgency/MirageAgency/settings.py
+++ MirageAgency/MirageAgency/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Statistics.apps.StatisticsConfig',
+    'MirageAgency.Statistics.apps.StatisticsConfig',
 ]
 
 MIDDLEWARE = [

f376accee5fc2ad2b71a7d9c1995c252e5842d94
Alexandr <sasha.v720@gmail.com>
2024-05-12 11:17:46 +0300

    add icons, css style

--- MirageAgency/MirageAgency/settings.py
+++ MirageAgency/MirageAgency/settings.py
@@ -18,7 +18,6 @@ from whitenoise import WhiteNoise
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASE_URL = 'postgresql://postgres:Fa6EGadc5GA63DG1dgFa6FaaEAGGg45d@roundhouse.proxy.rlwy.net:45322/railway'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -78,16 +77,16 @@ WSGI_APPLICATION = 'MirageAgency.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+  'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

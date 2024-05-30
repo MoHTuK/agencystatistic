5a03a581c22849c6633cba68eac66bf9e5a8d472
Alexandr <sasha.v720@gmail.com>
2024-05-31 00:27:09 +0300

    add new js files, some css and html rework

--- MirageAgency/mailbot/views.py
+++ MirageAgency/mailbot/views.py
@@ -8,9 +8,9 @@ import requests
 def mailbot(request):
     # Base URL
     base_url = 'https://goldenbride.net'
-
     # Login URL
     login_url = f'{base_url}/goldenbride/services/login'
+
 
     login_data = {
         'username': request.user.username,
@@ -20,8 +20,8 @@ def mailbot(request):
     login_response = session.post(login_url, data=login_data)
     images_url = f'{base_url}/usermodule/services/agencyhelper?command=attach'
     images_url_response = session.get(images_url).json()
-    img_data = []
+    img_list = []
     for item in images_url_response['list']:
-        img_data.append({"image_id": item["imageId"], 'image_url': item['imageUrls']['smallSizeUrl']})
+        img_list.append({"image_id": item["imageId"], 'image_url': item['imageUrls']['smallSizeUrl']})
 
-    return render(request, 'mailbot/main.html', context={'img_list': img_data})
+    return render(request, 'mailbot/main.html', context={'img_list': img_list})

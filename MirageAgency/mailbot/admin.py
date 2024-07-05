738c547ea3e02375c65f8d75d9811f75c6b12e4b
Alexandr <sasha.v720@gmail.com>
2024-07-05 23:48:31 +0300

    add admin panel info and copy btn for admin

--- MirageAgency/mailbot/admin.py
+++ MirageAgency/mailbot/admin.py
@@ -7,6 +7,8 @@ from .models import *
 @admin.register(Blacklist)
 class Blacklist_Admin(admin.ModelAdmin):
     list_display = ['lady_id', 'man_id']
+    list_filter = ('lady_id',)
+    search_fields = ['lady_id__username']
 
 
 @admin.register(GoldMan)

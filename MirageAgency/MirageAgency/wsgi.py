275d9e99194399ca53c72d8d0cb686976015f7af
Alex <sasha.v720@gmail.com>
2025-04-29 20:37:50 +0300

    Resolve merge conflicts

--- MirageAgency/MirageAgency/wsgi.py
+++ MirageAgency/MirageAgency/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MirageAgency.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MirageAgency.MirageAgency.settings')
 
 application = get_wsgi_application()
